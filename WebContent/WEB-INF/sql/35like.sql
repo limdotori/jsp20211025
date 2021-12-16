88991c402f7f0b454159eef26a09764ad9d916b3
limdotori <89769868+limdotori@users.noreply.github.com>
2021-12-16 17:57:48 +0900

    2021/12/01

---
+++ WebContent/WEB-INF/sql/35like.sql
@@ -0,0 +1,37 @@
+USE test;
+SELECT * FROM Customers
+WHERE CustomerName = 'Alfreds Futterkiste';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'Alf%'; -- % : 아무거나 0개이상 일치
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'A%'; -- % : 아무거나 0개이상 일치
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%A';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%A%';
+
+-- 연습1 : Employees 에서 LastName 에 'u'포함된 직원들 조회
+SELECT * FROM Employees
+WHERE LastName LIKE '%u%';
+-- 연습2 : Employees 에서 Notes 에 'BA'가 포함된 직원들 조회
+SELECT * FROM Employees
+WHERE Notes LIKE '%BA%';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'Ernst Hande_'; -- _ : 한글자 아무거나 
+
+
+-- 연습3 : jdbc10 -> dao의 쿼리 변경해서 동작하도록.
+SELECT employeeID, lastName, firstName, birthDate, notes
+FROM Employees
+WHERE note LIKE ?
+      OR lastName LIKE ?
+      OR firstName LIKE ?
+
+;
+
+
---
+++ WebContent/WEB-INF/sql/36in.sql
@@ -0,0 +1,54 @@
+USE test;
+SELECT * FROM Customers
+WHERE Country = 'Germany'
+    OR Country = 'Mexico'
+    OR Country = 'USA'
+;
+
+SELECT * FROM Customers
+WHERE Country IN ('Germany', 'Mexico', 'USA');
+
+SELECT * FROM Customers
+WHERE Country NOT IN ('Germany', 'Mexico', 'USA');
+
+
+-- 연습1 : 국가가 'USA', 'UK'인 Supplier들 조회 (IN 키워드 사용)
+SELECT * FROM Suppliers WHERE Country IN ('usa', 'uk');
+-- 연습2 : 국가가 'USA', 'UK'가 아닌 Supplier들 조회 (NOT IN 키워드 사용)
+SELECT * FROM Suppliers WHERE Country NOT IN ('usa', 'uk');
+
+SELECT * FROM Products;
+SELECT * FROM Categories;
+
+-- 연습3 : Meat 또는 Seafood 카테고리에 속한 상품명 조회
+
+SELECT * FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID
+WHERE CategoryName LIKE 'Meat%' OR CategoryName = 'Seafood'
+;
+-- 내가 문제 푼 방식은 IN ->
+SELECT * FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID
+WHERE CategoryName IN ('Meat/Poultry', 'Seafood')
+;
+
+-- SUBQUERY 활용
+SELECT * FROM Categories WHERE CategoryName IN  ('Meat/Poultry', 'Seafood');
+
+SELECT * FROM Products WHERE CategoryID IN (6, 8);
+
+SELECT * FROM Products 
+WHERE CategoryID IN ( SELECT CategoryID FROM Categories WHERE CategoryName IN  ('Meat/Poultry', 'Seafood') );
+
+-- 연습4 : Supplier가 있는 나라에 살고 있는 고객 목록 조회
+SELECT * FROM Customers 
+WHERE Country IN(SELECT Country 
+				 FROM Suppliers);
+
+
+SELECT country
+FROM Suppliers;
+
+SELECT * 
+FROM Customers
+WHERE Country IN (SELECT country 
+                  FROM Suppliers)
+;
